49cb5dd13b732dd1cafa4dc93f595d384c05179e
abdullah-icw <131659375+abdullah-icw@users.noreply.github.com>
2023-05-10 23:48:06 +0600

    download.php

--- download.php
+++ download.php
@@ -38,3 +38,33 @@ if(isset($_POST['submit'])) {
     
     exit();
 }
+
+<?php
+
+if(isset($_POST['submit'])) {
+    
+    $url = $_POST['url'];
+    $quality = $_POST['quality'];
+    
+    // check if youtube-dl is installed
+    if (!shell_exec('which youtube-dl')) {
+        echo "<p class='error-message'>Sorry, this feature is not available on the server.</p>";
+        exit;
+    }
+    
+    // set the download command
+    $command = 'youtube-dl --format ' . $quality . ' -o "videos/%(title)s.%(ext)s" ' . escapeshellarg($url);
+    
+    // execute the download command
+    exec($command, $output, $status);
+    
+    // check if the download was successful
+    if ($status === 0) {
+        echo "<p class='success-message'>The video has been downloaded successfully!</p>";
+        echo "<a href='videos/" . $output[0] . "'>Download Video</a>";
+    } else {
+        echo "<p class='error-message'>Sorry, there was an error downloading the video.</p>";
+    }
+}
+
+?>
